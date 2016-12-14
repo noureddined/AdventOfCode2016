8b34ebc8f892b34e7b72d4116a8c3206b71c9b31
noureddined <noureddined@users.noreply.github.com>
2016-12-14 15:10:09 +0100

    Create day3.py

---
+++ day-3/day3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+f  = open('input.txt','r')
+
+
+inputlist = f.read().replace("    ", "  ")
+inputlist = inputlist.replace("  ",",")
+
+dict = inputlist.split("\n")
+
+counter = 0
+for i in range(len(dict)):
+
+    a = dict[i].split(",")[0].replace(" ","")
+    b = dict[i].split(",")[1].replace(" ","")
+    c = dict[i].split(",")[2].replace(" ","")
+
+    a = int(a)
+    b = int(b)
+    c = int(c)
+
+    sum = (a+b)
+
+    if a+b > c and b+c > a and a+c > b:
+        counter = counter + 1
+        print (counter)
